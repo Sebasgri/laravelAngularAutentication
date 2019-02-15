1a8e7cb8b879402b5ff68f34d22e11a334377fe5
Sebasgri <grisebas@gmail.com>
2019-02-15 16:20:37 -0500

    Created components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { IngresarComponent } from './components/ingresar/ingresar.component';
+import { CerrarComponent } from './components/cerrar/cerrar.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { RequestResetComponent } from './components/password/request-reset/request-reset.component';
+import { ResponseResetComponent } from './components/password/response-reset/response-reset.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    IngresarComponent,
+    CerrarComponent,
+    PerfilComponent,
+    RequestResetComponent,
+    ResponseResetComponent
   ],
   imports: [
     BrowserModule
